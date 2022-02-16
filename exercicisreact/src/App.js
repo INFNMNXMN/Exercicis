ced353160bf5a258c9aadc73eb79b403bbed681f
LAB1INF-21\Administrador <isabel.navarro.fuentes@gmail.com>
2022-02-16 20:44:17 +0100

    Graella 1

--- exercicisreact/src/App.js
+++ exercicisreact/src/App.js
@@ -1,10 +1,12 @@
 import Calculadora from "./Calculadora/Calculadora";
+import Graella from "./Graella/Graella";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Calculadora />
+      {/* <Calculadora /> */}
+      <Graella />
     </div>
   );
 }
